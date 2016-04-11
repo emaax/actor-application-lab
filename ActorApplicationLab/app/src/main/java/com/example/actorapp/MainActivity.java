ed729dc6bf0de11a823b49caa69063e707f0bd99
Em Ax <emilia.axen@gmail.com>
2016-04-11 13:41:16 -0700

    actors added

--- ActorApplicationLab/app/src/main/java/com/example/actorapp/MainActivity.java
+++ ActorApplicationLab/app/src/main/java/com/example/actorapp/MainActivity.java
@@ -8,25 +8,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    ActorAdapter actorAdapter;
+    List<Actor> actors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        actors = new ArrayList<>();
+        actors.add(new Actor("Leo", 1, "Some day"));
+        actors.add(new Actor("Jamey", 0, "hahaha"));
+        actors.add(new Actor("Brad Pitt", 1, "1962"));
+        actors.add(new Actor("Emilia Axen", 10, "1990"));
+        listView = (ListView) findViewById(R.id.actors_list_view_main);
+        actorAdapter = new ActorAdapter(MainActivity.this, R.layout.activity_actors_list, actors);
+        listView.setAdapter(actorAdapter);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(fab != null){
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
